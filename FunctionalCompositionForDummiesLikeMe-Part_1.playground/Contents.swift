e7d2437f2d6ac9634c4a878d1f8ccce2fcba6ec7
Chuck Krutsinger <chuck.krutsinger@countermind.com>
2019-03-13 18:18:51 -0600

    added license and copyright notice

--- FunctionalCompositionForDummiesLikeMe-Part_1.playground/Contents.swift
+++ FunctionalCompositionForDummiesLikeMe-Part_1.playground/Contents.swift
@@ -1,5 +1,20 @@
 /*:
+ ## Copyright Notice MIT License from https://opensource.org/licenses/MIT
+ 
+ Copyright 2019 Chuck Krutsinger
+ 
+ Portions also Copyright 2018 Pointfree.co where indicated
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
  # Functional Composition in Swift for Dummies Like Me - Part 1
+
+ 
+ ## Introduction
  
  This will be the first in a series of posts hoping to help Swift programmers who are less experienced in functional programming be able to apply functional techniques to write better software. I will do so with a minimum of jargon, especially jargon from category theory which can be very hard to penetrate.
  
